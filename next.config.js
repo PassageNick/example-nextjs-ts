cd77bc39b6c96cf569e2f486eb76d701a3509335
Nick Hodges <nick.hodges@passage.id>
2023-01-12 17:41:36 -0500

    Removing appID (and if you are trying to use this, the application is deleted! Ha!)

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  PASSAGE_APP_ID: '2FUXhLY7QCkq9kXCsig5geOO'
+  PASSAGE_APP_ID: 'YOUR APP ID GOES HERE'
 
 }
 
