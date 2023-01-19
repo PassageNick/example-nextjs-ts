29a4d40e01b740ab57e42e5bb347ae1d307c5c49
Nick Hodges <nick.hodges@passage.id>
2023-01-19 09:54:33 -0600

    Adding "Typescript" to header

--- Components/layout/header/index.tsx
+++ Components/layout/header/index.tsx
@@ -6,7 +6,7 @@ const Header = () => {
 
         <div className={styles.mainHeader}>
             <a href="https://passage.id/" ><div className={styles.passageLogo}></div></a>
-            <div className={styles.headerText}>Passage + Next.js Example App</div>
+            <div className={styles.headerText}>Passage + Next.js/Typescript Example App</div>
             <div className={styles.spacer}></div>
             <a href="https://passage.id/" className={styles.link}>Go to Passage</a>
         </div>
